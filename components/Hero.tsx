376082512f4fc0ea16fc1f0e3ffb773b797cffda
Suciu Marius <110302280+sumarius56@users.noreply.github.com>
2022-10-09 18:54:39 +0300

    GREATER QUALITY UPDATE

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import { PageInfo } from "../typings";
 import BackgroundCircles from "./BackgroundCircles";
 import { urlFor } from "../sanity";
+import { Howl } from "howler";
+import { motion } from "framer-motion";
 
 type Props = {
   pageInfo: PageInfo;
 };
 
 function Hero({ pageInfo }: Props) {
+  const [openModal, setOpenModal] = useState(true);
   const [text, count] = useTypewriter({
     words: [
       'console.log("Hello World!")',
@@ -20,9 +23,61 @@ function Hero({ pageInfo }: Props) {
     loop: true,
     delaySpeed: 2000,
   });
+
+  const soundSource = "https://www.mboxdrive.com/ES_Clutch%20-%20STRLGHT.mp3";
+
+  const playMusic = (src) => {
+    const sound = new Howl({
+      src,
+      html5: true,
+    });
+    sound.play();
+  };
+
+  function handleClick() {
+    setOpenModal(false);
+    playMusic(soundSource);
+  }
+
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+    <div className="relative h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
+      {openModal && (
+        <div
+         
+          className="flex flex-col absolute bg-black/90 w-full h-screen  items-center justify-center z-50"
+        >
+          <motion.p
+           initial={{ opacity: 0, y: -100 }}
+          transition={{
+            duration: 1.5,
+          }}
+          whileInView={{ opacity: 1, y: 0 }}
+          className="text-4xl animate-pulse p-10">
+            Are you ready for a{" "}
+            <span className="decoration-[#F7AB0A]/50 underline">
+              top notch experience
+            </span>
+            ?
+          </motion.p>
+          <motion.button
+            initial={{ opacity: 0, y: 100 }}
+            transition={{
+              duration: 1.5,
+            }}
+            whileInView={{ opacity: 1, y: 0 }}
+            className="text-6xl text-[#F7AB0A]/40 hover:scale-125 hover:text-[#F7AB0A] "
+            onClick={handleClick}
+          >
+            YES!
+          </motion.button>
+        </div>
+      )}
+
+      {/* Music */}
+
+      {/* Music */}
+
       <img
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
         src={urlFor(pageInfo?.heroImage).url()}
--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,6 @@ type Props = {
 const Home = ({ pageInfo, projects, skills, socials, experiences }: Props) => {
   return (
     <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 ">
-      
       <Head>
         <title>{"<dev Suciu/>"}</title>
         <meta name="description" content="My super duper new portfolio" />
@@ -35,6 +34,7 @@ const Home = ({ pageInfo, projects, skills, socials, experiences }: Props) => {
       </Head>
 
       <Header socials={socials} />
+      
 
       <section id="hero" className="snap-start">
         <Hero pageInfo={pageInfo} />
